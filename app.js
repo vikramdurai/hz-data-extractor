5aca8f6a6e2aced1d4e1831201a1cec16dc69f79
vikramdurai <vikramvdurai@gmail.com>
2022-07-06 13:49:41 +0530

    v0.0.6 added support for other types of lists

--- app.js
+++ app.js
@@ -20,6 +20,31 @@ function waitForElm(selector) {
     });
 }
 
+function isVisible(elem) {
+    if (!(elem instanceof Element)) throw Error('DomUtil: elem is not an element.');
+    const style = getComputedStyle(elem);
+    if (style.display === 'none') return false;
+    if (style.visibility !== 'visible') return false;
+    if (style.opacity < 0.1) return false;
+    if (elem.offsetWidth + elem.offsetHeight + elem.getBoundingClientRect().height +
+        elem.getBoundingClientRect().width === 0) {
+        return false;
+    }
+    const elemCenter   = {
+        x: elem.getBoundingClientRect().left + elem.offsetWidth / 2,
+        y: elem.getBoundingClientRect().top + elem.offsetHeight / 2
+    };
+    if (elemCenter.x < 0) return false;
+    if (elemCenter.x > (document.documentElement.clientWidth || window.innerWidth)) return false;
+    if (elemCenter.y < 0) return false;
+    if (elemCenter.y > (document.documentElement.clientHeight || window.innerHeight)) return false;
+    let pointContainer = document.elementFromPoint(elemCenter.x, elemCenter.y);
+    do {
+        if (pointContainer === elem) return true;
+    } while (pointContainer = pointContainer.parentNode);
+    return false;
+}
+
 var candidates = [];
 
 async function getDataFromCandidatePage() {
@@ -36,17 +61,30 @@ async function getDataFromCandidatePage() {
         // name: await gD(".artdeco-entity-lockup__title")
     }
 
-    // Go the the Messages tab
-    document.querySelectorAll('.navigation-list__item')[2].click();
-    // Click on message-threads
-    (await waitForElm(".message-threads-list__list-item--link")).click();
-    await waitForElm(".message-list-entity__date")
-    let inMailDates = document.querySelectorAll(".message-list-entity__date");
+    
+    let messagesTab = document.querySelectorAll('.navigation-list__item')[2];
+    let cName = [...document.querySelectorAll('[data-live-test-row-lockup-full-name]')].filter(isVisible)[0].innerText.trim();
+    let currentDatetime = new Date().toLocaleString();
+    let contacted = "-";
+    let responded = "-";
+    let status = "Uncontacted";
+
+    if (Number(messagesTab.innerText[10]) >= 1) {
+        messagesTab.click();
+        status = (await gD(".message-state-entity__text"));
+        // Click on message-threads
+        (await waitForElm(".message-threads-list__list-item--link")).click();
+        await waitForElm(".message-list-entity__date")
+        let inMailDates = document.querySelectorAll(".message-list-entity__date");
+        contacted = inMailDates[0].innerText.trim();
+        responded = (status == "Accepted")? inMailDates[1].innerText.trim() : "-";
+    }
     return candidates[candidates.push({...profileData, ...{
-        status: document.querySelector(".message-state-entity__text").innerText.trim(),
-        contacted: inMailDates[0].innerText.trim(),
-        responded: inMailDates[1].innerText.trim(),
-        name: document.querySelectorAll(".message-list-entity__sender-name")[1].innerText.trim()
+        status: status,
+        contacted: contacted,
+        responded: responded,
+        name: cName,
+        dateAccessed: currentDatetime,
     }}) - 1]
 }
 
@@ -71,9 +109,9 @@ chrome.runtime.onMessage.addListener((m, s, reply) => {
 });
 
 function saveCandidatesAsCSV() {
-    let csvText = "Name,Status,Contacted,Responded,Project\n";
+    let csvText = "Name,Status,Contacted,Responded,Project,Date Accessed\n";
     candidates.forEach(c => {
-        csvText += `"${c.name}","${c.status}","${c.contacted}","${c.responded}","${c.project}"\n`;
+        csvText += `"${c.name}","${c.status}","${c.contacted}","${c.responded}","${c.project}","${c.dateAccessed}"\n`;
     })
     return csvText
 }
